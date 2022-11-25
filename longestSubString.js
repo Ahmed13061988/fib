28d45de7aff1a3f37b0d508bf3f07f36e3c9a831
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-24 20:30:20 -0700

    longest substring solution

--- longestSubString.js
+++ longestSubString.js
@@ -1,3 +1,17 @@
 // Given a string s, find the length of the longest
 // substring
 //  without repeating characters.
+
+var lengthOfLongestSubstring = function (s) {
+  if (s.length < 2) return s.length;
+  let max = 1;
+  const counts = { [s[0]]: 1 };
+  let i = 0,
+    j = 0;
+  while (++j < s.length) {
+    counts[s[j]] = (counts[s[j]] || 0) + 1;
+    while (counts[s[j]] > 1) counts[s[i++]]--;
+    max = Math.max(max, j - i + 1);
+  }
+  return max;
+};
