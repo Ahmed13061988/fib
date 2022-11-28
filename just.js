f8289903f78ae7f06f104090ee9f8dafee9c62ac
Ahmed Hussein <wolktheline@yahoo.com>
2022-11-27 17:23:01 -0700

    third

--- just.js
+++ just.js
@@ -3,3 +3,10 @@ const arr = ["Ahmed", "Ali", "Hussein"];
 arr.forEach((name) => console.log(name));
 
 const splitted = arr.map((name) => name.split());
+
+const sliced = arr.slice(0, 1);
+console.log(sliced);
+
+arr.splice(3, 0, "Alubaidi");
+
+console.log(arr);
