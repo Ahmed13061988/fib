318627740083bbeffbd7cef63a5476edaef514e9
Ahmed Hussein <wolktheline@yahoo.com>
2022-12-03 20:25:20 -0700

    longest common prefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,3 +1,17 @@
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 // If there is no common prefix, return an empty string "".
+
+var longestCommonPrefix = function (strs) {
+  var str = strs.sort();
+  var last = str[str.length - 1],
+    first = str[0],
+    i = 0;
+  var result = "";
+  for (i = 0; i < first.length; i++) {
+    if (first[i] === last[i]) {
+      result += first[i];
+    }
+  }
+  return result;
+};
